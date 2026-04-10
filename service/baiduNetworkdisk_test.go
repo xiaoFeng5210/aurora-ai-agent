136d3b9f53ebdaafcdf265e9bc24cdf768ae421b
Eadweard <aurora.coder.zqf@gmail.com>
2026-04-10 14:21:19 +0800

    fix

--- service/baiduNetworkdisk_test.go
+++ service/baiduNetworkdisk_test.go
@@ -10,14 +10,6 @@ func init() {
 	godotenv.Load("../.env")
 }
 
-func TestBaiduNetworkdiskUpload(t *testing.T) {
-	err := GMBaiduNetworkdiskUpload("/test.md", 0)
-	if err != nil {
-		t.Fatalf("BaiduNetworkdiskUpload failed: %v", err)
-	}
-	t.Logf("BaiduNetworkdiskUpload success")
-}
-
 func TestGetBaiduNetworkdiskFileList(t *testing.T) {
 	resp, err := GetBaiduNetworkdiskFileList("/oss/")
 	if err != nil {
@@ -27,14 +19,4 @@ func TestGetBaiduNetworkdiskFileList(t *testing.T) {
 		t.Fatalf("GetBaiduNetworkdiskFileList failed: %v", resp.Errno)
 	}
 	t.Logf("resp: %v", resp.List)
-}
-
-
-func TestHandleFile(t *testing.T) {
-	blockList, size, _, err := HandleFile()
-	if err != nil {
-		t.Fatalf("HandleFile failed: %v", err)
-	}
-	t.Logf("blockList: %v", blockList)
-	t.Logf("size: %d", size)
 }
