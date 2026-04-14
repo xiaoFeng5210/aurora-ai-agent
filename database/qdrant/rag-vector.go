cd032a5f093e01c16eb6632b605ae46787dbf19d
Eadweard <aurora.coder.zqf@gmail.com>
2026-04-14 14:29:32 +0800

    Refactor CreateRagCollection and UploadRagVector functions: Simplify collection creation by removing the collection name parameter and enhance vector upload functionality by adding user ID and file name to the payload. Update points creation to use chunks instead of documents.

--- database/qdrant/rag-vector.go
+++ database/qdrant/rag-vector.go
@@ -6,37 +6,69 @@ import (
 	"github.com/qdrant/go-client/qdrant"
 )
 
+var ragCollectionName = "rag"
+var embeddingModel = "sentence-transformers/all-minilm-l6-v2"
+
+
 // 创建集合
-func CreateRagCollection(collectionName string) error {
-	err := qdrantClient.CreateCollection(context.Background(), &qdrant.CreateCollection{
-		CollectionName: collectionName,
+func CreateRagCollection() error {
+	exists, err := qdrantClient.CollectionExists(context.Background(), ragCollectionName)                                                                
+  if err != nil {                                                                                                                                   
+    return err
+  }                                                                                                                                                 
+  if exists { 
+    return nil
+  }
+	return qdrantClient.CreateCollection(context.Background(), &qdrant.CreateCollection{
+		CollectionName: ragCollectionName,
 		VectorsConfig: qdrant.NewVectorsConfig(&qdrant.VectorParams{
 			Size:     1024,  // Vector size is defined by used model
 			Distance: qdrant.Distance_Cosine,
 		}),
 	})
-	return err
 }
 
-// 上传
-func UploadRagVector(collectionName string, docs []string) {
-	embeddingModel := "sentence-transformers/all-minilm-l6-v2"
+// 
+
+// upsert engine, 每个文本chunk组成一个point
+func UploadRagVector(chunks []string, userID int, fileName string) {
 	points := []*qdrant.PointStruct{}
-	for idx, doc := range docs {
+	for idx, chunk := range chunks {
 		points[idx] = &qdrant.PointStruct{
 			Id: qdrant.NewIDNum(uint64(idx)),
 			Vectors: qdrant.NewVectorsDocument(&qdrant.Document{
-				Text:  doc,
+				Text:  chunk,
 				Model: embeddingModel,
 			}),
 			Payload: qdrant.NewValueMap(map[string]any{
-				"text": doc,
+				"user_id": userID,
+				"text": chunk,
+				"file_name": fileName,
 			}),
 		}
 	}
 	qdrantClient.Upsert(context.Background(), &qdrant.UpsertPoints{
-		CollectionName: collectionName,
+		CollectionName: ragCollectionName,
 		Points:         points,
 	})
 }
 
+
+// query engine
+// func QueryRagVector(query string, userID int) {
+// 	queryResult, err := qdrantClient.Query(context.Background(), &qdrant.QueryPoints{
+// 		CollectionName: ragCollectionName,
+// 		Query: qdrant.NewQueryDocument(&qdrant.Document{
+// 			Text:  query,
+// 			Model: embeddingModel,
+// 		}),
+// 		Limit: qdrant.PtrOf(uint64(3)),
+// 		Filter: &qdrant.Filter{
+// 			Must: []*qdrant.Condition{
+// 				qdrant.NewRange("user_id", &qdrant.Range{
+// 					Match: qdrant.PtrOf(userID),
+// 				}),
+// 			},
+// 		},
+// 	})
+// }
